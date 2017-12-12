63898fccf4b583bbd71eba3638b3398bde78c8e2
Brendan O'Handley <brendan.ohandley@gmail.com>
2017-12-11 21:25:29 -0600

    write sorting method

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,2 +1,17 @@
-define bubble_sort(array)
+def bubble_sort(array)
+  loop do 
+    changed = false
+    array.each_with_index do |el, index|
+      if array[index+1] && el > array[index + 1]  
+        plus = array[index + 1]
+        array[index + 1] = el
+        array[index] = plus
+        changed = true
+      end
+    end
+    break if changed == false
+  end
+  array
 end
+
+p bubble_sort([1,3,2])
